7c1642ce687d1388f3f911842969a9ef32cff395
Victor Martinez <victor.eod2@gmail.com>
2022-05-18 21:54:32 -0700

    messing with card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import './App.css';
 import { Card } from './components/Card';
 
+const movie = {
+    title: "The Departed",
+    imgUrl: "https://a.ltrbxd.com/resized/sm/upload/jr/th/pu/pb/laefkgrfa3oKwvBtWTBtf2suiI4-0-230-0-345-crop.jpg?k=6dfd45925a" ,
+    director: "Martin Scorsese",
+    description: "Very good movie directed by Martin Scorsese starring Leonardo DiCaprio, Matt Damon, Mark Wahlberg, and Jack Nicholson."
+};
+
 function App() {
   return (
       <div className="App">
-          <Card title="The Departed" />
+          <Card
+            title = {movie.title}
+            imgUrl = {movie.imgUrl}
+            director = {movie.director}
+            description = {movie.description}
+          />
       </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,17 @@ import React from "react";
 
 type CardProps = {
     title: string;
+    imgUrl: string;
+    director: string;
+    description: string;
 }
-export const Card = ( { title }: CardProps): JSX.Element => <div>{ title }</div>;
+
+export const Card = ( { title, imgUrl, director, description }: CardProps): JSX.Element => 
+    <div>
+        <img src={imgUrl}/>
+        <h2>{ title }</h2>
+        <h3>Directed by { director } </h3>
+        <p> { description } </p>
+    </div>
+;
 
